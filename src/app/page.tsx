46cb6b4c7d2bd2b7ee198e249c72b47855ccb605
Christovan Queiroz <christovanbrumqueiroz@gmail.com>
2023-08-25 17:17:50 -0300

    feat: commit inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,164 @@
+import React from 'react'
+
 import Image from 'next/image'
+
+import Logo from '../../public/Logo.svg'
+import Avatar from '../../public/Avatar.svg'
+import Mail from '../../public/Mail.svg'
+import Github from '../../public/Github.svg'
+import Linkedin from '../../public/Linkedin.svg'
+import Html from '../../public/Html.svg'
+import Css from '../../public/Css.svg'
+import ReactLogo from '../../public/ReactLogo.svg'
+import Js from '../../public/Js.svg'
+import LeftChevron from '../../public/LeftChevron.svg'
+import RightChevron from '../../public/RightChevron.svg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center gap-12 bg-gradient-to-b from-cyan-600 to-teal-900 text-white sm:p-24 md:p-12 lg:flex-row p-8 justify-evenly xl:p-0">
+      <div className="text-center">
+        <div className='flex items-center justify-center gap-4 sm:flex-col md:flex-row'>
+          <Image 
+            className='w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24'
+            src={Logo}
+            alt='Logo'
+          />
+
+          <div className='flex flex-col mt-4 lg:mt-2'>
+            <span className='text-2xl md:text-4xl lg:text-3xl'>
+              Christovan Queiroz
+            </span>
+
+            <p className='text-lg mb-4 md:text-2xl lg:text-2xl'>
+              Desenvolvedor Front-end
+            </p>
+          </div>
+        </div>
+
+        <div className='flex justify-center'>
+          <Image className='bg-white border-4 border-white rounded-full w-72 h-72 max-sm:w-[180px] max-sm:h-[180px] md:w-[300px] md:h-[300px] max-xl:w-[120px] max-xl:h-[120px]'
+            src={Avatar}
+            alt='Foto'
+          />
+        </div>
+
+        <div className='max-sm:flex flex-row justify-center max-sm:mt-4'>
+          <div className='flex flex-col md:text-xl'>
+            <a className='flex gap-2 items-center justify-start p-2 cursor-default hover:underline' href="mailto:christovanbrumqueiroz@gmail.com">
+              <Image
+                src={Mail}
+                alt='E-mail'
+                width={32}
+                height={32}
+              />
+
+              <span className='max-sm:hidden'>
+                christovanbrumqueiroz@gmail.com
+              </span>
+            </a>
+          </div>
+
+          <div className='flex flex-cols sm:ml-12 md:text-xl ml-0'>
+            <a className='flex gap-2 items-center justify-start p-2 cursor-default hover:underline' target='_blank' href="https://github.com/christovanqueiroz/">
+              <Image
+                src={Github}
+                alt='Github'
+                width={32}
+                height={32}
+              />
+              <span className='max-sm:hidden'>
+                github.com/christovanqueiroz
+              </span>
+            </a>
+          </div>
+
+          <div className='flex flex-col sm:ml-24 md:text-xl ml-0'>
+            <a className='flex gap-2 items-center justify-start p-2 cursor-default hover:underline' target='_blank' href="https://linkedin.com/in/christovan-queiroz">
+              <Image
+                src={Linkedin}
+                alt='Linkedin'
+                width={32}
+                height={32}
+              />
+              <span className='max-sm:hidden'>
+                linkedin.com/in/christovan-queiroz
+              </span>
+            </a>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className='flex flex-col'>
+        <div className='flex'>
+          <div>
+            <Image
+              src={Html}
+              alt='Html'
+              width={48}
+              height={48}
+            />
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div>
+            <Image 
+              src={Css}
+              alt='Css'
+              width={48}
+              height={48}
+            />
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className='flex'>
+          <div>
+            <Image 
+              src={LeftChevron}
+              alt='LeftChevron'
+              width={48}
+              height={48}
+            />
+          </div>
+          
+          <div className='border-2 border-white rounded-lg w-48 h-32 sm:w-56 sm:h-40 md:w-80 md:h-48 lg:w-96 lg:h-72'>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div>
+            <Image 
+              src={RightChevron}
+              alt='RightChevron'
+              width={48}
+              height={48}
+            />
+          </div>
+        </div>
+
+        <div className='flex'>
+          <div>
+            <Image 
+              src={ReactLogo}
+              alt='ReactLogo'
+              width={48}
+              height={48}
+            />
+          </div>
+
+          <div>
+            <Image 
+              src={Js}
+              alt='Js'
+              width={48}
+              height={48}
+            />
+          </div>
+        </div>
+
+        <div>
+          <button>
+            Hover me
+          </button>
+        </div>
       </div>
     </main>
   )
